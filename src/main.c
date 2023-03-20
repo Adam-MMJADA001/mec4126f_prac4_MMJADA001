7af463c6eaecd1c0db479b24368af02a2d45865e
Adam-MMJADA001 <mmjada001@myuct.ac.za>
2023-03-21 01:48:09 +0200

    1.1 display_on_LCD Function

--- src/main.c
+++ src/main.c
@@ -29,7 +29,7 @@ void main(void)
 	init_LCD();		//used to initiate the LCD
 	init_LEDs();
 	init_switches();
-	display_on_LCD(0);
+	display_on_LCD(0); //displays 0 on reset
 
 	while(1)
 	{
@@ -62,37 +62,6 @@ void display_on_LCD(uint8_t count){
 	char output[3];
 	sprintf(output, "%d", count);
 	lcd_putstring(output);
-
-}
-
-void init_LEDs(void){
-
-	RCC->AHBENR|= RCC_AHBENR_GPIOBEN;
-
-	GPIOB->MODER |=(GPIO_MODER_MODER0_0|
-					GPIO_MODER_MODER1_0|
-					GPIO_MODER_MODER2_0|
-					GPIO_MODER_MODER3_0|
-					GPIO_MODER_MODER4_0|
-					GPIO_MODER_MODER5_0|
-					GPIO_MODER_MODER6_0|
-					GPIO_MODER_MODER7_0);
-}
-
-void display_on_LEDs(uint8_t count){
-	GPIOB -> ODR &= ~(GPIOB -> ODR);
-	GPIOB-> ODR = count;
-}
-
-void init_switches(void){
-
-	RCC->AHBENR|= RCC_AHBENR_GPIOAEN;
-
-	GPIOA->MODER &= ~(GPIO_MODER_MODER1|
-					  GPIO_MODER_MODER2);
-
-	GPIOA->PUPDR |= (GPIO_PUPDR_PUPDR1_0|
-					 GPIO_PUPDR_PUPDR2_0);
 
 }
 
